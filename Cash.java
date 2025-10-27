39d5ed5fb409ea0df49903be9baa840a52111756
kris-keygen <kristiankeaganl@gmail.com>
2025-10-27 19:13:09 +0700

    Cash Class

---
+++ Cash.java
@@ -0,0 +1,21 @@
+package week08.kristian.id.ac.umn;
+
+public class Cash extends Payment {
+
+    public Cash(Item item) {
+        super(item);
+    }
+
+    @Override
+    public int pay() {
+        if (isPaidOff()) {
+            return 0;
+        }
+        isPaidOff = true;
+        return this.item.getPrice();
+    }
+
+    public String getClassName() {
+        return "CASH";
+    }
+}
