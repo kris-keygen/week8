1629a934f1a29b27bb106a9258ce42bb42385267
kris-keygen <kristiankeaganl@gmail.com>
2025-10-27 18:38:38 +0700

    Abstract class Payment

--- Payment.java
+++ Payment.java
@@ -1,5 +1,15 @@
 package week08.kristian.id.ac.umn;
 
-public class Payment {
-
+public abstract class Payment {
+	protected double amount;
+	
+	public Payment(double amount) {
+		this.amount= amount;	
+	}
+	
+	abstract void processPayment();
+	
+	public void paymentDetails() {
+		System.out.println("Processing Payment of $" + amount);
+	}
 }
